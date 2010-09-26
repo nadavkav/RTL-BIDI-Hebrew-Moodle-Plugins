32643903800faa4c4d1b036884b935b9dd9707ec
Nadav Kavalerchik <nadavkav@gmail.com>
2010-09-26 20:21:37 +0200

    course : format : topoverblk - course overview over left blocks column

---
+++ course/format/topoverblk/lang/en_utf8/format_topoverblk.php
@@ -0,0 +1,4 @@
+<?php
+$string['nametopoverblk'] = 'Unit name';
+$string['formattopoverblk'] = 'Overview section with sideblock column';
+?>
---
+++ course/format/topoverblk/lang/he_utf8/format_topoverblk.php
@@ -0,0 +1,4 @@
+<?php
+$string['nametopoverblk'] = 'פסקאת מבוא בראש הדף';
+$string['formattopoverblk'] = 'פסקאת מבוא בראש הדף';
+?>
