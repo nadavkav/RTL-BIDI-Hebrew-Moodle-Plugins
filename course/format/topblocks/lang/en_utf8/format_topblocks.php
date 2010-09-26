8bf906d46ec04ca86e1c63b270c7a574a7f84321
Nadav Kavalerchik <nadavkav@gmail.com>
2010-09-26 20:19:03 +0200

    course : format : topblocks - blocks on the course header

---
+++ course/format/topblocks/lang/en_utf8/format_topblocks.php
@@ -0,0 +1,4 @@
+<?php
+$string['nametopblocks'] = 'Unit name';
+$string['formattopblocks'] = '4 Blocks above a scratched page layout';
+?>
---
+++ course/format/topblocks/lang/he_utf8/format_topblocks.php
@@ -0,0 +1,4 @@
+<?php
+$string['nametopblocks'] = 'יחידות הוראה';
+$string['formattopblocks'] = 'משבצות ניהול בראש המרחב';
+?>
