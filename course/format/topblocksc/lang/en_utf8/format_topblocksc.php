63ec61428ca5fbf4851cbeaaa5674c1a30c36ee1
Nadav Kavalerchik <nadavkav@gmail.com>
2010-09-26 20:19:26 +0200

    course : format : topblocksc - centered blocks on the course header

---
+++ course/format/topblocksc/lang/en_utf8/format_topblocksc.php
@@ -0,0 +1,4 @@
+<?php
+$string['nametopblocksc'] = 'Unit name';
+$string['formattopblocksc'] = '3 Blocks above a fixed-width centered page';
+?>
---
+++ course/format/topblocksc/lang/he_utf8/format_topblocksc.php
@@ -0,0 +1,4 @@
+<?php
+$string['nametopblocksc'] = 'יחידות הוראה';
+$string['formattopblocksc'] = 'משבצות ניהול ממורכזות בראש המרחב';
+?>
