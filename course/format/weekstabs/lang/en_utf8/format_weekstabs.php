6f01da7819116846038036e378765ee2fbf78f66
Nadav Kavalerchik <nadavkav@gmail.com>
2010-10-15 14:04:29 +0200

    course format - weekstabs (supports english only courses)

---
+++ course/format/weekstabs/lang/en_utf8/format_weekstabs.php
@@ -0,0 +1,11 @@
+<?PHP // $Id: format_weekstabs.php,v 1.1 2008/11/08 22:26:26 arborrow Exp $ 
+      // months.php - created with Moodle 1.9 
+      
+$string['formatweekstabs'] = 'Weekly tab format';
+$string['monthhide'] = 'Hide months';
+$string['monthlyoutline'] = 'Monthly outline';
+$string['monthshow'] = 'Show months';
+$string['showallmonths'] = 'Show all months';
+$string['showonlymonth'] = 'Show only month';
+
+?>
