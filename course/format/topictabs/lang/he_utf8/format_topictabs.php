de3f5c1b678e640e1be07c30aa409ae2a5a78eb6
Nadav Kavalerchik <nadavkav@gmail.com>
2010-10-15 14:04:11 +0200

    course format - topictabs (supports english only courses)

---
+++ course/format/topictabs/lang/he_utf8/format_topictabs.php
@@ -0,0 +1,11 @@
+<?PHP // $Id: format_weekstabs.php,v 1.1 2008/11/08 22:26:26 arborrow Exp $ 
+      // months.php - created with Moodle 1.9 
+      
+$string['formattopictabs'] = 'תצורת מבנה נושאים עם חוצצים';
+$string['monthhide'] = 'הסתרת חודשים';
+$string['monthlyoutline'] = 'מסגרת חודשים';
+$string['monthshow'] = 'הצגת חודשים';
+$string['showallmonths'] = 'הצגת כל החודשים';
+$string['showonlymonth'] = 'הצגת חודש זה בלבד';
+
+?>
