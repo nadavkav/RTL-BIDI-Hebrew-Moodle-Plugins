8b2ca871c0762ada31b421cb2cddd277b5e67d91
Nadav Kavalerchik <nadavkav@gmail.com>
2010-11-28 14:02:46 +0200

    question - imgedit - add suport for invoking Pixlr.com UI according to current user's language

---
+++ question/type/imagedit/lang/ar_utf8/qtype_imagedit.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The language strings for the imagedit question type.
+ *
+ * @author Nadav Kavalerchik
+ * based on the work of : Adriane Boyd (adrianeboyd@gmail.com) with fileresponse
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package aab_imagedit
+ *
+ */
+
+$string['addingimagedit'] = 'הוספת שאלת עריכת תמונה';
+$string['editingimagedit'] = 'שאלת עריכת תמונה';
+$string['imagedit'] = 'עריכת תמונת';
+$string['uploaderror'] = 'הקובץ לא הועלה לשרת בהצלחה, כיוון שלא נמצא או שהייה מעבר לגודל המותר.';
+$string['uploadedfile'] = 'הקובץ בתהליך העלאה לשרת';
+$string['uploadnew'] = 'החלפת הקובץ הקיים בשרת עם הקובץ החדש';
+$string['addessay'] = 'הוספת פסקאת תאור לתמונה';
+$string['openinnewwindow'] = 'מנהל קבצים ותמונות';
+$string['clicktoedit'] = 'הקליקו על התמונה כדי לערוך אותה <br/>לסיום, בחרות באפשרות \"שמירה\" מתוך תפריט \"קובץ\"';
+$string['saveimage'] = 'שמירת תמונה';
+$string['savebeforecontinue'] = '(לאחר שמירת התמונה, הקישו על המקש F5 במקלדת שלכם כדי לוודא שהתמונה אכן נשמרה במערכת, לפני שאתם ממשיכים)';
+
+?>
