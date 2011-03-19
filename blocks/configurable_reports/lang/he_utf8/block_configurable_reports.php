404f17b862bf26cddfa0dd104aa3eb05b21c6f17
Nadav Kavalerchik <nadavkav@gmail.com>
2011-03-19 18:47:34 +0200

    blocks configurable_reports (new) - with basic Hebrew translation support: courses, categories,graphs,filters,calculations,permissions...

---
+++ blocks/configurable_reports/lang/he_utf8/block_configurable_reports.php
@@ -0,0 +1,243 @@
+<?php
+
+$string['blockname'] = "דוחות מתקדמים";
+$string['report_courses'] = "דוחות קורס";
+$string['report_users'] = "דוחות משתמש";
+$string['report_sql'] = "דוחות SQL כלליים";
+$string['managereports'] = "ניהול דוחות";
+
+$string['report'] = "דוח";
+$string['reports'] = "דוחות";
+
+$string['columns'] = "עמודות";
+$string['conditions'] = "תנאים";
+$string['permissions'] = "הרשאות";
+$string['plot'] = "גרפים";
+$string['filters'] = "מסננים";
+$string['calcs'] = "חישובים";
+$string['ordering'] = "סידור";
+$string['customsql'] = "דוחות SQL מותאמים";
+$string['addreport'] = "הוספת דוח חדש";
+$string['type'] = "סוג דוח";
+$string['columncalculations'] = "חישובי עמודות";
+$string['newreport'] = "דוח חדש";
+$string['column'] = "עמודה";
+$string['confirmdeletereport'] = "Are you sure you want to delete this report?";
+$string['noreportsavailable'] = "No reports available";
+$string['downloadreport'] = "Download report";
+
+$string['configurable_reports:manageownreports'] = "Manage own reports";
+$string['configurable_reports:managereports'] = "Mange reports";
+$string['configurable_reports:managesqlreports'] = "Manage SQL reports";
+$string['configurable_reports:viewreports'] = "View reports";
+
+$string['exportoptions'] = "יצוא אפשרויות";
+$string['field'] = "שדה";
+
+// Report form
+$string['typeofreport'] = "סוג דוח";
+$string['enablejsordering'] = "סידור עמודות בעזרת JavaScript";
+$string['enablejspagination'] = "ניווט עמודים בעזרת JavaScript";
+$string['export_ods'] = "יצוא בתצורת ODS";
+$string['export_xls'] = "יצוא בתצורת XLS";
+$string['viewreport'] = "תצוגת דוח";
+$string['norecordsfound'] = "לא נמצאו רשומות המתאימות לדוח";
+
+// Columns
+$string['column'] = "עמודה";
+$string['nocolumnsyet'] = "No columns yet";
+$string['tablealign'] = "Table align";
+$string['tablecellspacing'] = "Table cellspacing";
+$string['tablecellpadding'] = "Table cellpadding";
+$string['tableclass'] = "Table class";
+$string['tablewidth'] = "Table width";
+$string['cellalign'] = "Cell align";
+$string['cellwrap'] = "Cell wrap";
+$string['cellsize'] = "Cell size";
+
+
+// Conditions
+$string['conditionexpr'] = "תנאי";
+$string['conditionexprhelp'] = "Enter a valid condition i.e: (c1 and c2) or (c4 and c3)";
+$string['noconditionsyet'] = "No conditions yet";
+$string['operator'] = "Operator";
+$string['value'] = "Value";
+
+// Filter
+$string['filter'] = "מסנן";
+$string['nofilteryet'] = "No filters yet";
+$string['courses'] = "Courses";
+$string['nofiltersyet'] = "No filters yet";
+
+// Calcs
+$string['nocalcsyet'] = "No calculations yet";
+
+// Plot
+$string['noplotyet'] = "No plots yet";
+
+// Permissions
+
+$string['nopermissionsyet'] = "No permissions yet";
+
+// Ordering
+
+$string['noorderingyet'] = "No ordering yet";
+$string['userfieldorder'] = "User field order";
+
+// Plugins
+$string['coursefield'] = "Course field";
+$string['ccoursefield'] = "Course field condition";
+$string['roleusersn'] = "Number of users with role...";
+$string['coursecategory'] = "Course in category";
+$string['filtercourses'] = "Courses";
+$string['filtercourses_summary'] = "This filter shows a list of courses. Only one course can be selected at the same time";
+$string['roleincourse'] = "User with the selected role/s in the current report course";
+$string['reportscapabilities'] = "Report Capabilities";
+$string['reportscapabilities_summary'] = "Users with the capability moodle/site:viewreports enabled";
+$string['sum'] = "Sum";
+$string['max'] = "Maximum";
+$string['min'] = "Minimum";
+$string['average'] = "Average";
+$string['pie'] = "Pie";
+$string['piesummary'] = "A pie graph";
+$string['pieareaname'] = "Name";
+$string['pieareavalue'] = "Value";
+$string['piesummary'] = "A pie graph";
+
+$string['anyone'] = "Anyone";
+$string['anyone_summary'] = "Any user in the Campus will be able to view this report";
+
+$string['currentuserfinalgrade'] = "Current user final grade in course";
+
+$string['currentuserfinalgrade_summary'] = "This column shows the final grade of the current user in the row-course";
+$string['userfield'] = "User profile field";
+
+$string['cuserfield'] = "User field condition";
+$string['direction'] = "Direction";
+
+$string['courseparent'] = "Courses whose parent is";
+$string['coursechild'] = "Courses that are children of";
+
+$string['currentusercourses'] = "Current user enrolled courses";
+$string['currentusercourses_summary'] = "A list of the current users courses (only visible courses)";
+$string['currentreportcourse'] = "Current report course";
+$string['currentreportcourse_summary'] = "The course where the report has been created";
+
+$string['coursefieldorder'] = "Course field order";
+
+$string['fcoursefield'] = "Course field filter";
+$string['usersincoursereport'] = "Any user in the current report course";
+
+$string['groupvalues'] = "Group same values (sum)";
+$string['fuserfield'] = "User field filter";
+
+$string['module'] = "Module";
+
+$string['usersincurrentcourse'] = "Users in current report course";
+$string['usersincurrentcourse_summary'] = "Users with the role/s selected in the report course";
+
+$string['usermodoutline'] = "User module outline stats";
+$string['donotshowtime'] = "Do not show date information";
+$string['usermodactions'] = "User module actions";
+
+$string['currentuser'] = "Current user";
+$string['currentuser_summary'] = "The user that is viewing the report";
+
+$string['puserfield'] = "User field value";
+$string['puserfield_summary'] = "User with the selected value in the selected field";
+
+$string['startendtime'] = "Start / End date filter";
+$string['starttime'] = "Start date";
+$string['endtime'] = "End date";
+
+$string['template'] = "תבנית";
+$string['availablemarks'] = "Available marks";
+$string['header'] = "Header";
+$string['footer'] = "Footer";
+$string['templaterecord'] = "Record template";
+$string['querysql'] = "SQL Query";
+$string['filterstartendtime_summary'] = "Start / End date filter";
+
+$string['pagination'] = "ניווט בדפים";
+$string['disabled'] = "לא פעיל";
+$string['enabled'] = "פעיל";
+
+$string['reportcolumn'] = "Other report column";
+
+$string['reporttable'] = "Report table";
+$string['columnandcellproperties'] = "Column and cell properties";
+$string['componenthelp'] = "Component help";
+
+$string['badsize'] = 'Incorrect size, it must be in &#37; or px';
+$string['badtablewidth'] = 'Incorrect width, it must be in &#37; or absolute value';
+$string['missingcolumn'] = "A column is required";
+$string['error_operator'] = "Operator not allowed";
+
+$string['error_field'] = "Field not allowed";
+$string['error_value_expected_integer'] = "Expected integer value";
+$string['badconditionexpr'] = "Incorrect condition expression";
+
+$string['notallowedwords'] = "Not allowed words";
+$string['nosemicolon'] = "No semicolon";
+$string['noexplicitprefix'] = "No explicit prefix";
+$string['queryfailed'] = "Query failed";
+$string['norowsreturned'] = "No rows returned";
+
+$string['listofsqlreports'] = '<a href=\"http://docs.moodle.org/en/ad-hoc_contributed_reports\" target=\"_blank\">List of SQL Contributed reports</a>';
+
+$string['usersincoursereport_summary'] = "Any user in the current report course";
+
+$string['printreport'] = 'הדפסת דוח';
+
+$string['importreport'] = "יבוא דוח";
+$string['exportreport'] = "יצוא דוח";
+
+$string['download'] = "יצוא דוחות";
+
+$string['report_timeline'] = 'דוח ציר זמן';
+$string['timeline'] = 'ציר זמן';
+$string['timemode'] = 'Time mode';
+$string['previousdays'] = 'Previous days';
+$string['fixeddate'] = 'Fixed date';
+$string['previousstart'] = 'Previous start';
+$string['previousend'] = 'Previous end';
+$string['forcemidnight'] = 'Force midnight';
+$string['timeinterval'] = 'Time interval';
+$string['date'] = 'Date';
+$string['dateformat'] = 'Date format';
+$string['customdateformat'] = 'Custom date format';
+$string['custom'] = 'Custom';
+
+$string['line'] = 'Line graph';
+$string['userstats'] = 'User statistics';
+$string['stat'] = 'Statistic';
+$string['statslogins'] = 'Logins in the platform';
+$string['activityview'] = 'Activity views';
+$string['activitypost'] = 'Activity posts';
+$string[''] = '';
+$string['globalstatsshouldbeenabled'] = 'Site statistics must be enabled. Go to Admin -> Server -> Statistics';
+
+$string['xaxis'] = 'X Axis';
+$string['yaxis'] = 'Y Axis';
+$string['serieid'] = 'Serie column';
+$string['groupseries'] = 'Group series';
+$string['linesummary'] = 'A line graph with multiple series of data';
+
+$string['coursestats'] = 'Course stats';
+$string['statstotalenrolments'] = 'Total enrolments';
+$string['statsactiveenrolments'] = 'Active (last week) enrolments';
+$string['youmustselectarole'] = 'At least a role is required';
+
+$string['report_categories'] = 'דוח קטגוריות';
+$string['categoryfield'] = 'Category field';
+$string['categoryfieldorder'] = 'Category field order';
+$string['categories'] = 'Categories';
+$string['parentcategory'] = 'Parent category';
+$string['filtercategories'] = 'Filter categories';
+$string['filtercategories_summary'] = 'To filter by category';
+
+$string['includesubcats'] = 'Include subcategories';
+
+$string['coursededicationtime'] = 'Course dedication time';
+
+?>
---
+++ blocks/configurable_reports/styles.php
@@ -0,0 +1,4 @@
+#blocks-configurable_reports-editcomp.dir-rtl form.mform div.felement,
+#blocks-configurable_reports-editcomp.dir-rtl form.mform fieldset.felement {
+    direction: ltr;
+}
