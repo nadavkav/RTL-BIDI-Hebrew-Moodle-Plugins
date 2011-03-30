50b0e8190291fe8abb838b65befeb8720fea7114
Nadav Kavalerchik <nadavkav@gmail.com>
2011-03-31 00:11:05 +0200

    blocks choice_to_group : add support for adding new groups without deleting old ones

--- blocks/choice_to_group/block_ctg_action.php
+++ blocks/choice_to_group/block_ctg_action.php
@@ -37,10 +37,10 @@ if (!defined('MAGPIE_OUTPUT_ENCODING')) {
 
 
 if (!empty($id)) {
-    // we get the complete $course object here because print_header assumes this is 
+    // we get the complete $course object here because print_header assumes this is
     // a complete object (needed for proper course theme settings)
     $course = get_record('course', 'id', $id);
-} 
+}
 
 $straddedit = get_string('mode1_action', 'block_choice_to_group');
 
@@ -51,69 +51,68 @@ if (!empty($course)) {
     $navigation = $straddedit;
 }
 
-print_header($straddedit, $straddedit, $navigation);
-
-
+print_header($straddedit, $straddedit);//, $navigation);
 
 echo "<h1>".get_string("is_processing","block_choice_to_group")."</h1>";
 
 print_box_start(); //Start frame for background
 
 if ( $action_id == 1 ){
-	
-	
-	$groupids = get_groups($id); // Get all groups in course
-	if ($groupids){
-		foreach ($groupids as $groupid) groups_delete_group($groupid->id); //delete the old groups and members
-		echo "<h2>".get_string("deletion_successful","block_choice_to_group")."</h2>";
-	}
-	
-	$options = get_records_sql("SELECT id, text FROM {$CFG->prefix}choice_options where choiceid=$selection"); //get options from selected choice (=groupnames)
+  $groupids = get_groups($id); // Get all groups in course
+  if ($groupids){
+    foreach ($groupids as $groupid) groups_delete_group($groupid->id); //delete the old groups and members
+    echo "<h2>".get_string("deletion_successful","block_choice_to_group")."</h2>";
+  }
+}
 
-	$groups = array();
-	
-	foreach ($options as $option){
-		$dataobject_group->name = $option->text;
-		$dataobject_group->courseid = $id ;						//assign course-id to dataobject
-		
-		//version check
-		if ($release < "1.9") {
-			$groups[$option->text] = groups_create_group($id,$dataobject_group);
-		}
-		else {
-			$groups[$option->text] = groups_create_group($dataobject_group);
-		}
-		
-		echo $dataobject_group->name."<BR>";
-	}
-	echo "<h2>".get_string("group_creation_successful","block_choice_to_group")."</h2>";
+if ( $action_id >= 1 ){
 
-	$choices = get_records_sql("SELECT userid, optionid FROM {$CFG->prefix}choice_answers where choiceid=$selection");
-	if ($choices){
-		foreach ($choices as $choice) { 
-			$dataobject_member_group->userid = $choice->userid;					//store id of user that made this choice
+  $options = get_records_sql("SELECT id, text FROM {$CFG->prefix}choice_options where choiceid=$selection"); //get options from selected choice (=groupnames)
 
-			$groupname = $options[$choice->{optionid}]->{text};					//get title of choice (=groupname) from choice_options-table
-			$usernames = get_records_sql("SELECT id, firstname, lastname FROM {$CFG->prefix}user where id={$choice->userid}");
-			
-			echo get_string("add_user","block_choice_to_group")." <STRONG>".
-			     $usernames[$choice->{userid}]->{firstname}." ".
-			     $usernames[$choice->{userid}]->{lastname}." </STRONG>".
-			     get_string("to_group","block_choice_to_group")." <STRONG>".$groupname."</STRONG> ".
-			     get_string("hinzu","block_choice_to_group")."<BR>";
+  $groups = array();
 
-			$dataobject_member_group->groupid = $groups[ $groupname ];
-			$check = insert_record("groups_members", $dataobject_member_group);
-			if (!$check) break;
-			
-		}
-		if ( $check ) {
-			echo "<h2>".get_string("user_assignment_successful","block_choice_to_group")."</h2>";
-		}
-	}
-	else {
-		echo "<h2>".get_string("no_users","block_choice_to_group")."</h2>";
-	}
+  foreach ($options as $option){
+    $dataobject_group->name = $option->text;
+    $dataobject_group->courseid = $id ;						//assign course-id to dataobject
+
+    //version check
+    if ($release < "1.9") {
+      $groups[$option->text] = groups_create_group($id,$dataobject_group);
+    }
+    else {
+      $groups[$option->text] = groups_create_group($dataobject_group);
+    }
+
+    echo $dataobject_group->name."<BR>";
+  }
+  echo "<h2>".get_string("group_creation_successful","block_choice_to_group")."</h2>";
+
+  $choices = get_records_sql("SELECT userid, optionid FROM {$CFG->prefix}choice_answers where choiceid=$selection");
+  if ($choices){
+    foreach ($choices as $choice) {
+      $dataobject_member_group->userid = $choice->userid;					//store id of user that made this choice
+
+      $groupname = $options[$choice->{optionid}]->{text};					//get title of choice (=groupname) from choice_options-table
+      $usernames = get_records_sql("SELECT id, firstname, lastname FROM {$CFG->prefix}user where id={$choice->userid}");
+
+      echo get_string("add_user","block_choice_to_group")." <STRONG>".
+            $usernames[$choice->{userid}]->{firstname}." ".
+            $usernames[$choice->{userid}]->{lastname}." </STRONG>".
+            get_string("to_group","block_choice_to_group")." <STRONG>".$groupname."</STRONG> ".
+            get_string("hinzu","block_choice_to_group")."<BR>";
+
+      $dataobject_member_group->groupid = $groups[ $groupname ];
+      $check = insert_record("groups_members", $dataobject_member_group);
+      if (!$check) break;
+
+    }
+    if ( $check ) {
+      echo "<h2>".get_string("user_assignment_successful","block_choice_to_group")."</h2>";
+    }
+  }
+  else {
+    echo "<h2>".get_string("no_users","block_choice_to_group")."</h2>";
+  }
 }
 print_box_end();
 echo "<form action=\"".$CFG->wwwroot."/course/view.php?id=".$id."\" method=\"post\" id=\"block_rss\">";
