147308c410a0dbfac24a2732c0b547df1f2c784e
Nadav Kavalerchik <nadavkav@gmail.com>
2011-05-07 19:58:41 +0300

    mod/map: bigger description text area

--- mod/map/mod_form.php
+++ mod/map/mod_form.php
@@ -13,28 +13,28 @@ require_once ($CFG->dirroot.'/course/moodleform_mod.php');
 class mod_map_mod_form extends moodleform_mod {
 
 	function definition() {
-		 
+
 		global $CFG;
 		$mform    =& $this->_form;
 
 		$mform->addElement('header', 'general', get_string('general', 'form'));
-			
+
 			$mform->addElement('text', 'name', get_string('mapname', 'map'), array('size'=>'64'));
 			$mform->setType('name', PARAM_TEXT);
 			$mform->addRule('name', null, 'required', null, 'client');
 		//the google map key must have been entered with the module settings for maps to work
 		if(map_config_ok()){
 			//-------------------------------------------------------------------------------
-			
 
-			$mform->addElement('htmleditor', 'text', get_string('maptext', 'map'));
+
+			$mform->addElement('htmleditor', 'text', get_string('maptext', 'map'), array('rows' => '24'));
 			$mform->setType('text', PARAM_RAW);
 			$mform->addRule('text', null, 'required', null, 'client');
 			$mform->setHelpButton('text', array('writing', 'questions', 'richtext'), false, 'editorhelpbutton');
 
 			$mform->addElement('format', 'format', get_string('format'));
 
-			
+
 			$menuoptions=array();
 			$menuoptions[0] = get_string('no');
 			$menuoptions[1] = get_string('yes');
@@ -62,7 +62,7 @@ class mod_map_mod_form extends moodleform_mod {
 			//don't allow adding/editing of maps without key
 			$mform->setElementError('name',get_string("badconfig","map"));
 		}
-	
+
 	}
 
 }
