143a8f9564f34711abc7c64c6ae8ddc1c439e360
Nadav Kavalerchik <nadavkav@gmail.com>
2011-05-28 23:11:10 +0300

    course format hiddenmod: Topics - Indented Modules are Hidden

---
+++ course/format/hiddenmod/lang/en_utf8/format_hiddenmod.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Nadav Kavalerchik.
+ * User: nadavkav
+ * Date: 5/28/11
+ * Time: 10:40 PM
+ * Hiddenmod course format
+ */
+
+$string['formathiddenmod'] = 'Topics - Indented Modules are Hidden';
+
+?>
---
+++ course/format/hiddenmod/lang/he_utf8/format_hiddenmod.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Nadav Kavalerchik.
+ * User: nadavkav
+ * Date: 5/28/11
+ * Time: 10:40 PM
+ * Hiddenmod course format
+ */
+
+$string['formathiddenmod'] = 'נושאים - פעילויות מוזחות מוסתרות';
+
+?>
