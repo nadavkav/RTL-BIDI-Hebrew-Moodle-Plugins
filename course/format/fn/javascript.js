a57ae3c010bfc083d042fa3ccfe0673d81f87267
Nadav Kavalerchik <nadavkav@gmail.com>
2011-12-31 21:23:23 +0200

    course/format/fn - use Theme's colors

--- course/format/fn/javascript.js
+++ course/format/fn/javascript.js
@@ -11,8 +11,12 @@ function getStyle(x,styleProp)
 
 var elm = document.getElementsByClassName('header');
 var fnelm = document.getElementsByClassName('fnweeklynavselected');
+var bgcolor = getStyle(elm[0],'background-color');
+var color = getStyle(elm[0],'color');
+if ( bgcolor == null or bgcolor = rgba(0,0,0,0) ) bgcolor = beige;
 for (i=0;i<fnelm.length;i++) {
-  fnelm[i].style.background = getStyle(elm[0],'background-color');
+  fnelm[i].style.background = bgcolor;
+  fnelm[i].style.color = color;
 }
 
 </script>
