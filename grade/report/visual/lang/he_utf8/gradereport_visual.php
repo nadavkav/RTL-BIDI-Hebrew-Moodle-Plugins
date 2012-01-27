c834fd09131952f71354f08e5f8bbda1a43b2b6c
Nadav Kavalerchik <nadavkav@gmail.com>
2012-01-27 21:45:50 +0200

    grade/report/visual - New Visual Course Grade report (using Flash)

---
+++ grade/report/visual/lang/he_utf8/gradereport_visual.php
@@ -0,0 +1,88 @@
+<?php
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// NOTICE OF COPYRIGHT                                                   //
+//                                                                       //
+// Moodle - Modular Object-Oriented Dynamic Learning Environment         //
+//          http://moodle.org                                            //
+//                                                                       //
+// Copyright (C) 1999 onwards  Martin Dougiamas  http://moodle.com       //
+//                                                                       //
+// This program is free software; you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// This program is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details:                          //
+//                                                                       //
+//          http://www.gnu.org/copyleft/gpl.html                         //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
+$string['modulename'] = 'דוחות התפלגות ציונים גרפיים';
+$string['visual:view'] = 'צפייה ב: דוחות התפלגות ציונים גרפיים';
+
+$string['visualselector'] = 'דוח נוכחי:';
+$string['nogroup'] = 'ללא קבוצות';
+$string['allgroups'] = 'כל הקבוצות';
+$string['printable'] = 'תצוגה להדפסה';
+
+/// settings
+$string['prefgeneral'] = 'כללי';
+$string['prefcalc'] = 'חישובים';
+$string['aggregationview'] = 'תצוגה מסכמת';
+$string['aggregationposition'] = 'מיקום הסיכום';
+$string['incompleasmin'] = 'ציונים חסרים כערך בסיס';
+$string['usehidden'] = 'מספר הציונים החסרים';
+$string['uselocked'] = 'מספר הציונים הנעולים';
+$string['showscaleitems'] = 'תצוגה על־פי סולם־ערכים';
+$string['showvalueitems'] = 'תצוגה על־פי ערך מספרי';
+
+/// buttons text
+$string['hide'] = 'הסתרה';
+$string['show'] = 'תצוגה';
+$string['xlabels'] = 'ציר אופקי';
+$string['ylabels'] = 'ציר אנכי';
+$string['axes'] = 'צירים';
+$string['invertaxes'] = 'היפוך צירים';
+
+/// common axis labels
+$string['grade'] = 'ציון';
+$string['student'] = 'תלמיד';
+$string['item'] = 'פעילות';
+$string['numberstudents'] = 'מספר תלמידים';
+$string['percentstudents'] = 'אחוז תלמידים';
+
+/// grades_vs_students strings
+$string['gradesvsstudents'] = 'נִרְמֵול ציונים מול תלמידים';
+$string['gradesvsstudents:title'] = 'נִרְמֵול ציונים מול תלמידים';
+$string['visual:vis:grades_vs_students'] = 'תצוגה גרפית של תלמידים מול ציונים';
+
+/// grade_distribution strings
+$string['gradedistribution:title'] = 'התפלגות ציונים';
+$string['gradedistribution'] = 'התפלגות ציונים (גרף קווי)';
+$string['visual:vis:grade_distribution'] ='צפייה ב: התפלגות ציונים (גרף קווי)';
+
+/// grade_distribution_bar strings
+$string['gradedistributionbar'] = 'התפלגות ציונים על־פי פעילות (גרף עמודות)';
+$string['visual:vis:grade_distribution_bar'] = 'צפייה ב: התפלגות ציונים על־פי פעילות (גרף עמודות)';
+
+/// grade_distribution_group_bar
+$string['gradedistributiongroupbar'] = 'התפלגות ציונים על־פי קבוצות (גרף עמודות)';
+$string['visual:vis:grade_distribution_group_bar'] = 'צפייה ב: התפלגות ציונים על־פי קבוצות (גרף עמודות)';
+
+/// continuous_grade_distribution
+$string['continuousgradedistribution'] = 'רצף התפלגות ציונים';
+$string['visual:vis:continuous_grade_distribution'] = 'צפייה ב: רצף התפלגות ציונים';
+
+/// grades_vs_items
+$string['gradesvsitems'] = 'ממוצע ציונים מול פעילויות';
+$string['gradesvsitems:title'] = 'ממוצע ציונים מול פעילויות';
+$string['visual:vis:grades_vs_items'] = 'צפייה ב: ממוצע ציונים מול פעילויות';
+
+$string['backtoreports'] = 'חזרה לדוח משובים';
+
+?>
