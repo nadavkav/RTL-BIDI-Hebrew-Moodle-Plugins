f335cf0042f391e583043f5844c9224deeed4ab5
Nadav Kavalerchik <nadavkav@gmail.com>
2012-01-27 21:35:25 +0200

    course/format/dblistview - New course format that display Database activities' List View on the frontpage of the course

---
+++ course/format/dblistview/lang/he_utf8/format_dblistview.php
@@ -0,0 +1,7 @@
+<?php
+
+$string['formatdblistview'] = 'תצוגת תבניות רשימה מתוך בסיס נתונים';
+$string['namedblistview'] = 'יחידת הוראה';
+$string['more'] = 'מידע נוסף...';
+
+?>
---
+++ course/format/dblistview/styles.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by Nadav Kavalerchik.
+ * eMail: nadavkav@gmail.com
+ * Date: 12/6/11
+ * Description:
+ *    Change me...
+ */
+
+?>
+
+/*.dblistview .topics .side {display:none;}*/
+.dblistview .course-content {width: 960px;margin: 1px auto;}
+.dblistview .sideblock .header .hide-show-image {margin-left: 7px;margin-top: 0.45em;}
+
+/*
+.dblistview td#left-column {width: 150px !important;}
+.dblistview td#right-column {width: 320px !important;}
+*/
+
+.dblistview .summary {
+border-top: 2px solid black;
+font-size-adjust: 0.7;
+padding: 5px;
+}
+
+#course-view .dblistview .section .activity > a {
+background-color:#F5E49F; text-decoration: none;
+//font-size-adjust: 0.6;
+font-weight: bold;padding:1px;
+-webkit-border-radius: 3px;
+-moz-border-radius: 3px;
+border-radius: 3px;
+padding-right:5px;
+padding-left:5px;
+}
+#course-view .dblistview .section .activity > a:hover {background-color:#FCF0A7; text-decoration: none;border:1px dashed black;padding:1px;}
+.course-content > .summary {background:none;}
+
+.dblistview .section.separator {height:20px;}
