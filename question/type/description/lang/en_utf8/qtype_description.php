7266f95df0db0e08a5b0883c253a7f0dfe8a7115
Nadav Kavalerchik <nadavkav@gmail.com>
2012-02-01 00:55:24 +0200

    question/type/description - (core) patch - floating follow me window overlay

---
+++ question/type/description/lang/en_utf8/qtype_description.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Nadav Kavalerchik.
+ * eMail: nadavkav@gmail.com
+ * Date: 1/29/12
+ * Description:
+ *    Change me...
+ */
+
+$string['followme'] = 'Follow Me';
+$string['pin'] = 'Pin';
+$string['stop'] = 'Stop';
---
+++ question/type/description/lang/he_utf8/qtype_description.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Nadav Kavalerchik.
+ * eMail: nadavkav@gmail.com
+ * Date: 1/29/12
+ * Description:
+ *    Change me...
+ */
+
+$string['followme'] = 'עקוב אחריי';
+$string['pin'] = 'מקובע לדף';
+$string['stop'] = 'חזרה לשאלון';
