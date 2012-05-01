384889abf6a35689903ef1270922859cc8fb9a41
Nadav Kavalerchik <nadavkav@gmail.com>
2012-05-01 22:22:10 +0300

    question/type/order (minor) Hebrew translation update

--- question/type/order/lang/he_utf8/qtype_order.php
+++ question/type/order/lang/he_utf8/qtype_order.php
@@ -2,13 +2,11 @@
       // qtype_order.php - created with Moodle 1.9.2 (Build: 20080716) (2007101520)
       // local modifications from http://www.tikshuv.org.il/moodle
 
-
 $string['addingorder'] = 'הוספת שאלת מיון פריטים';
 $string['addmoreqblanks'] = 'הוספת פריטים חדשים';
 $string['defaultresponse'] = 'ללא תשובה';
 $string['editingorder'] = 'עריכת שאלת מיון פריטים';
-$string['filloutthreeitems'] = '<BR>עליכם להזין לפחות שלוש פריטים בסדר תקין.
-פריטים ריקים לא מוצגים.';
+$string['filloutthreeitems'] = '<BR>עליכם להזין לפחות שלוש פריטים בסדר תקין. פריטים ריקים לא מוצגים.';
 $string['horizontal'] = 'הצגה אופקית של פריטים';
 $string['itemno'] = 'פריט $a';
 $string['order'] = 'מיון פריטים (בגרירה)';
