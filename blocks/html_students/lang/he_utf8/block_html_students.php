c11017804b2038f1c8157aa81e39e51189400b39
Nadav Kavalerchik <nadavkav@gmail.com>
2012-12-20 00:00:07 +0200

    blocks/html_students (new)

---
+++ blocks/html_students/lang/he_utf8/block_html_students.php
@@ -0,0 +1,9 @@
+<?php
+
+$string['html_students'] = 'תוכן מעוצב - לסטודנטים';
+$string['configtitle'] = 'כותרת';
+$string['configcontent'] = 'תוכן';
+$string['leaveblanktohide'] = 'השאירו ריק, להסתרת הכותרת';
+$string['newhtmlblock'] = 'טרם הוזנה כותרת';
+
+?>
